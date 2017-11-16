47441a9a74be427d6256f22dd107825313f4d0c3
Hein Htet <heinhtet.hh405@gmail.com>
2017-11-16 11:19:17 +0630

    Complete Larabiz

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -7,6 +7,11 @@ use App\Listing;
 
 class ListingsController extends Controller
 {
+
+    public function __construct() {
+       $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +19,8 @@ class ListingsController extends Controller
      */
     public function index()
     {
-        //
+        $listings = Listing::orderBy('created_at', 'desc')->get();
+        return view('listings')->with('listings', $listings);
     }
 
     /**
@@ -63,7 +69,8 @@ class ListingsController extends Controller
      */
     public function show($id)
     {
-        //
+      $listing = Listing::find($id);
+      return view('showlisting')->with('listing', $listing);
     }
 
     /**
@@ -74,7 +81,8 @@ class ListingsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $listing = Listing::find($id);
+        return view('editlisting')->with('listing', $listing);
     }
 
     /**
@@ -86,7 +94,24 @@ class ListingsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $this->validate($request, [
+          'name' => 'required',
+          'email' => 'email'
+      ]);
+
+      // Creat Listing
+      $listing = Listing::find($id);
+      $listing->name = $request->input('name');
+      $listing->website = $request->input('website');
+      $listing->email = $request->input('email');
+      $listing->phone = $request->input('phone');
+      $listing->address = $request->input('address');
+      $listing->bio = $request->input('bio');
+      $listing->user_id = auth()->user()->id;
+
+      $listing->save();
+
+      return redirect('/dashboard')->with('success', 'Listing Updated');
     }
 
     /**
@@ -97,6 +122,9 @@ class ListingsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $listing = Listing::find($id);
+        $listing->delete();
+
+        return redirect('/dashboard')->with('success', 'Listing Removed');
     }
 }
